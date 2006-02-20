06aa769a5cfa259f57ad7d84ceb4753640931ac9
Gregory D. Reddin <greddin@apache.org>
2006-02-20 23:40:16 +0000

    Ticket #38255.  Added TilesListener class to replace TilesServlet in 2.3+ environments. Patch submitted by Matt Raible.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk/tiles@379284 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/tiles/listener/TilesListener.java
@@ -0,0 +1,180 @@
+package org.apache.tiles.listener;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import org.apache.tiles.DefinitionsFactory;
+import org.apache.tiles.DefinitionsFactoryConfig;
+import org.apache.tiles.DefinitionsFactoryException;
+import org.apache.tiles.TilesUtil;
+import org.apache.tiles.TilesUtilImpl;
+
+/**
+ * @author mraible
+ * @version $Revision$ $Date$
+ */
+public class TilesListener implements ServletContextListener {
+
+    /**
+     * The logger for this class
+    */
+   protected static Logger logger = Logger.getLogger(TilesListener.class.
+                                                                      getName());
+
+
+    /**
+     * The default name of a context init parameter that specifies the Tiles configuration file
+    */
+    private static final String DEFAULT_CONFIG_FILE_PARAM = "definitions-config";
+
+
+    /**
+     * The default name of the Tiles configuration file
+    */
+    private static final String DEFAULT_CONFIG_FILE = "/WEB-INF/tiles.xml";
+
+
+    /**
+     * An error message stating that something went wrong during initialization
+    */
+    private static final String CANT_POPULATE_FACTORY_ERROR =
+         "CAN'T POPULATE TILES DEFINITION FACTORY";
+
+
+    /**
+     * The Tiles definition factory
+    */
+   protected DefinitionsFactory definitionFactory = null;
+
+
+    /**
+     * A comma-separated list of filenames representing the
+     * application's Tiles configuration files.
+    */
+    private String configFiles = null;
+
+    public void contextInitialized(ServletContextEvent event) {
+        logger.info("Initializing TilesListener");
+        configFiles = event.getServletContext().getInitParameter(DEFAULT_CONFIG_FILE_PARAM);
+
+        try {
+            ServletContext context = event.getServletContext();
+
+            // Create factory config object
+            DefinitionsFactoryConfig fconfig = readFactoryConfig(context);
+            fconfig.setModuleAware(false);
+
+            TilesUtil.setTilesUtil(new TilesUtilImpl());
+            initDefinitionsFactory(context, fconfig);
+        }
+        catch(Exception ex) {
+            saveExceptionMessage(event.getServletContext(), ex);
+                    throw new RuntimeException(ex.getMessage(), ex);
+        }
+
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+        this.definitionFactory = null;
+    }
+
+
+    /**
+     * Populates the tiles factory configuration. If a
+     * context init param named <i>definitions-config</i>
+     * was defined, that param's value is assumed to be
+     * a comma-separated list of configuration file names,
+     * all of which are processed. If a
+     * <i>definitions-config</i> context param was not
+     * specified, Tiles assumes that your Tiles definition
+     * file is <code>/WEB-INF/tiles.xml</code>.
+     */
+    protected DefinitionsFactoryConfig readFactoryConfig(ServletContext context)
+        throws ServletException {
+        DefinitionsFactoryConfig factoryConfig = new DefinitionsFactoryConfig();
+        Map map = new HashMap();
+
+        try {
+            if(configFiles != null) {
+                logger.info("CONFIG FILES DEFINED IN WEB.XML");
+               map.put(DEFAULT_CONFIG_FILE_PARAM, configFiles);
+           }
+            else {
+                logger.info("CONFIG FILES WERE NOT DEFINED IN WEB.XML, " +
+                              "LOOKING FOR " + DEFAULT_CONFIG_FILE);
+               map.put(DEFAULT_CONFIG_FILE_PARAM, DEFAULT_CONFIG_FILE);
+            }
+
+            populateConfigParameterMap(context, map);
+            factoryConfig.populate(map);
+        }
+        catch (Exception ex) {
+            saveExceptionMessage(context, ex);
+           throw new UnavailableException(CANT_POPULATE_FACTORY_ERROR + ex.getMessage());
+        }
+        return factoryConfig;
+    }
+
+
+    /**
+     * Initializes the Tiles definitions factory.
+     *
+     * @param servletContext The servlet context
+     * @param factoryConfig The definitions factory config
+     */
+   private void initDefinitionsFactory(ServletContext servletContext,
+                                       DefinitionsFactoryConfig factoryConfig)
+                                                    throws ServletException {
+        logger.info("initializing definitions factory...");
+        // Create configurable factory
+        try {
+            definitionFactory = TilesUtil.createDefinitionsFactory(
+                                            servletContext, factoryConfig);
+        } catch (DefinitionsFactoryException ex) {
+                    ex.printStackTrace();
+            throw new ServletException(ex.getMessage(), ex);
+        }
+    }
+
+
+    /**
+     * Stores the message associated with any exception thrown in this
+     * servlet in application scope. Tiles later accesses that message
+     * if an exception is thrown when the tiles:insert tag is
+     * activated.
+     *
+     * @param context The servlet configuration
+     * @param ex An exception
+     */
+    private void saveExceptionMessage(ServletContext context, Exception ex) {
+       logger.warning("Caught exception when initializing definitions factory");
+       logger.warning(ex.getMessage());
+       logger.warning(ex.toString());
+       context.setAttribute("TILES_INIT_EXCEPTION", ex.getMessage());
+    }
+
+    /**
+     * Populates a map with the parameters contained in the context configuration.
+     *
+     * @param context The servlet context
+     * @param paramMap The map to fill
+     */
+    private void populateConfigParameterMap(ServletContext context, Map paramMap) {
+        Enumeration enumeration;
+        String paramName;
+
+        enumeration = context.getInitParameterNames();
+        while (enumeration.hasMoreElements()) {
+            paramName = (String) enumeration.nextElement();
+            if (!paramMap.containsKey(paramName)) {
+                paramMap.put(paramName, context.getInitParameter(paramName));
+            }
+        }
+    }
+}
