954fe86734563d14bc333c54adb56e947d45eb59
Antonio Petrelli <apetrelli@apache.org>
2010-12-13 19:48:27 +0000

    TILESSB-38 Fixed tiles-autotag-jsp-runtime checkstyle.
    git-svn-id: https://svn.apache.org/repos/asf/tiles/sandbox/trunk@1045341 13f79535-47bb-0310-9956-ffa450edef68

--- tiles-autotag/tiles-autotag-jsp-runtime/src/main/java/org/apache/tiles/autotag/jsp/runtime/JspModelBody.java
+++ tiles-autotag/tiles-autotag-jsp-runtime/src/main/java/org/apache/tiles/autotag/jsp/runtime/JspModelBody.java
@@ -1,3 +1,23 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.tiles.autotag.jsp.runtime;
 
 import java.io.IOException;
@@ -9,10 +29,24 @@ import javax.servlet.jsp.tagext.JspFragment;
 
 import org.apache.tiles.autotag.core.runtime.AbstractModelBody;
 
+/**
+ * The body abstraction in a JSP tag.
+ *
+ * @version $Rev$ $Date$
+ */
 public class JspModelBody extends AbstractModelBody {
 
+    /**
+     * The real body.
+     */
     private JspFragment jspFragment;
 
+    /**
+     * Constructor.
+     *
+     * @param jspFragment The real body.
+     * @param jspContext The page context.
+     */
     public JspModelBody(JspFragment jspFragment, JspContext jspContext) {
         super(jspContext.getOut());
         this.jspFragment = jspFragment;
