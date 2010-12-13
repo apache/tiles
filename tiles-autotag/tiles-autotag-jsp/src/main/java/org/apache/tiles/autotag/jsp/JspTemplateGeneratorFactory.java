0c9d3621f64b402c0527c87bf1a2cd361b1d5667
Antonio Petrelli <apetrelli@apache.org>
2010-12-13 19:40:43 +0000

    TILESSB-38 Fixed tiles-autotag-jsp checkstyle.
    git-svn-id: https://svn.apache.org/repos/asf/tiles/sandbox/trunk@1045333 13f79535-47bb-0310-9956-ffa450edef68

--- tiles-autotag/tiles-autotag-jsp/src/main/java/org/apache/tiles/autotag/jsp/JspTemplateGeneratorFactory.java
+++ tiles-autotag/tiles-autotag-jsp/src/main/java/org/apache/tiles/autotag/jsp/JspTemplateGeneratorFactory.java
@@ -1,3 +1,23 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.tiles.autotag.jsp;
 
 import java.io.File;
@@ -7,6 +27,11 @@ import org.apache.tiles.autotag.generate.TemplateGeneratorBuilder;
 import org.apache.tiles.autotag.generate.TemplateGeneratorFactory;
 import org.apache.velocity.app.VelocityEngine;
 
+/**
+ * Creates a template generator to build JSP code around template models.
+ *
+ * @version $Rev$ $Date$
+ */
 public class JspTemplateGeneratorFactory implements TemplateGeneratorFactory {
 
     /**
@@ -19,12 +44,27 @@ public class JspTemplateGeneratorFactory implements TemplateGeneratorFactory {
      */
     private File resourcesOutputDirectory;
 
+    /**
+     * The Velocity engine.
+     */
     private VelocityEngine velocityEngine;
 
+    /**
+     * The template generator builder.
+     */
     private TemplateGeneratorBuilder templateGeneratorBuilder;
 
+    /**
+     * Constructor.
+     *
+     * @param classesOutputDirectory The directory where classes will be generated.
+     * @param resourcesOutputDirectory The directory where the TLD file will be generated.
+     * @param velocityEngine The Velocity engine.
+     * @param templateGeneratorBuilder The template generator builder.
+     */
     public JspTemplateGeneratorFactory(File classesOutputDirectory,
-            File resourcesOutputDirectory, VelocityEngine velocityEngine, TemplateGeneratorBuilder templateGeneratorBuilder) {
+            File resourcesOutputDirectory, VelocityEngine velocityEngine,
+            TemplateGeneratorBuilder templateGeneratorBuilder) {
         this.classesOutputDirectory = classesOutputDirectory;
         this.resourcesOutputDirectory = resourcesOutputDirectory;
         this.velocityEngine = velocityEngine;
--- tiles-autotag/tiles-autotag-jsp/src/main/java/org/apache/tiles/autotag/jsp/TLDGenerator.java
+++ tiles-autotag/tiles-autotag-jsp/src/main/java/org/apache/tiles/autotag/jsp/TLDGenerator.java
@@ -1,3 +1,23 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.tiles.autotag.jsp;
 
 import java.io.File;
@@ -7,21 +27,37 @@ import org.apache.tiles.autotag.generate.AbstractTemplateSuiteGenerator;
 import org.apache.tiles.autotag.model.TemplateSuite;
 import org.apache.velocity.app.VelocityEngine;
 
+/**
+ * Generates the TLD file, using a template suite.
+ *
+ * @version $Rev$ $Date$
+ */
 public class TLDGenerator extends AbstractTemplateSuiteGenerator {
 
+    /**
+     * Constructor.
+     *
+     * @param velocityEngine The Velocity engine.
+     */
     public TLDGenerator(VelocityEngine velocityEngine) {
         super(velocityEngine);
     }
 
-    protected String getTemplatePath(File directory, String packageName, TemplateSuite suite, Map<String, String> parameters) {
+    @Override
+    protected String getTemplatePath(File directory, String packageName,
+            TemplateSuite suite, Map<String, String> parameters) {
         return "/org/apache/tiles/autotag/jsp/tld.vm";
     }
 
-    protected String getFilename(File directory, String packageName, TemplateSuite suite, Map<String, String> parameters) {
+    @Override
+    protected String getFilename(File directory, String packageName,
+            TemplateSuite suite, Map<String, String> parameters) {
         return suite.getName() + "-jsp.tld";
     }
 
-    protected String getDirectoryName(File directory, String packageName, TemplateSuite suite, Map<String, String> parameters) {
+    @Override
+    protected String getDirectoryName(File directory, String packageName,
+            TemplateSuite suite, Map<String, String> parameters) {
         return "META-INF/tld/";
     }
 
