efb0974a736d10d2762a9d737e8028b9c025651b
Nicolas LE BAS <nlebas@apache.org>
2012-04-28 01:22:30 +0000

    TILES-547 #resolve disabled the use of cache for loading raw definitions (the cache contains only non-wildcard or resolved definitions)
    git-svn-id: https://svn.apache.org/repos/asf/tiles/framework/trunk@1331650 13f79535-47bb-0310-9956-ffa450edef68

--- tiles-parent/tiles-core/src/main/java/org/apache/tiles/definition/dao/CachingLocaleUrlDefinitionDAO.java
+++ tiles-parent/tiles-core/src/main/java/org/apache/tiles/definition/dao/CachingLocaleUrlDefinitionDAO.java
@@ -222,7 +222,7 @@ public class CachingLocaleUrlDefinitionDAO extends BaseLocaleUrlDefinitionDAO
         Locale parentLocale = LocaleUtil.getParentLocale(customizationKey);
         localeDefsMap = new LinkedHashMap<String, Definition>();
         if (parentLocale != null) {
-            Map<String, Definition> parentDefs = loadParentDefinitions(parentLocale);
+            Map<String, Definition> parentDefs = loadRawDefinitionsFromResources(parentLocale);
             if (parentDefs != null) {
                 localeDefsMap.putAll(parentDefs);
             }
